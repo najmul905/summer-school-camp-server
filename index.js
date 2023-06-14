00ccc1f8a10562de860d74429be0c190f63f2ac8
najmul905 <nh4808563@gmail.com>
2023-06-14 08:02:27 +0600

    create admin api and instructor api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.yq5wikg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,13 +29,57 @@ async function run() {
 const usersCollection=client.db("schoolDB").collection("users")
 const classCollection=client.db("schoolDB").collection("class")
 const instructorCollection=client.db("schoolDB").collection("instructor")
-
+// post user
 app.post('/users', async(req,res)=>{
     const item=req.body;
     const result=await usersCollection.insertOne(item)
     res.send(result)
 })
 
+// user update
+// create admin
+
+app.patch('/users/admin/:id',async(req,res)=>{
+  const id=req.params.id
+  const filter={_id: new ObjectId(id)}
+  const updateDoc={
+    $set:{
+      role:"admin"
+    }
+  }
+const result=await usersCollection.updateOne(filter,updateDoc);
+res.send(result)
+
+})
+// create instructor
+app.patch('/users/instructor/:id',async(req,res)=>{
+  const id=req.params.id
+  const filter={_id: new ObjectId(id)}
+  const updateDoc={
+    $set:{
+      role:"instructor"
+    }
+  }
+const result=await usersCollection.updateOne(filter,updateDoc);
+res.send(result)
+
+})
+
+// get User
+app.get('/users',async(req,res)=>{
+  const data=req.body
+  const result=await usersCollection.find().toArray()
+  res.send(result)
+})
+
+// post class
+app.post("/class",async(req,res)=>{
+  const item=req.body
+  const result=await classCollection.insertOne(item)
+  res.send(result)
+})
+
+// get class
 app.get('/class',async(req,res)=>{
     const result=await classCollection.find().toArray();
     res.send(result)
